0e0dbe1aaed1ed46872c53c9cb778283cc9fc84f
iasraful <iasraful321@gmail.com>
2023-11-27 19:49:06 +0600

    initial Project setup

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+/** @format */
+
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+//Parsers
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 12;
+
+  res.send(a);
+});
+
+export default app;
